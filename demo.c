ce72029ffb731c970ae79cb7de61bae8c4e35e68
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-08-27 18:45:07 -0400

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,18 +1,21 @@
 // Build: x86_64-w64-mingw32-gcc -s demo.c -nostdlib -nostartfiles -ffreestanding -fno-ident -Wl,-subsystem,windows -e _start -Os -fPIC -fno-asynchronous-unwind-tables -T linker.ld -o demo.exe
 
 #include "winapi_loader.h"
+#define STRING(name, value) __attribute__((section(".text"))) static char name[] = value;
 
+// -------------------- Function pointer type --------------------
 typedef int (WINAPI* MessageBoxA_t)(HWND, LPCSTR, LPCSTR, UINT);
 
-__attribute__((section(".text"))) static char user32_dll[]  = "user32.dll";
-__attribute__((section(".text"))) static char messageboxa[] = "MessageBoxA";
-__attribute__((section(".text"))) static char hello_msg[]   = "Hello from shellcode!";
-__attribute__((section(".text"))) static char title_msg[]   = "C Shellcode Demo";
+// -------------------- Strings --------------------
+STRING(user32_dll, "user32.dll")
+STRING(messageboxa, "MessageBoxA")
+STRING(hello_msg, "Hello from shellcode!")
+STRING(title_msg, "C Shellcode Demo")
 
+// -------------------- Entry point --------------------
 __attribute__((section(".text.start")))
 int _start(void) {
     HMODULE hUser32 = myLoadLibraryA(user32_dll);
     MessageBoxA_t pMessageBoxA = (MessageBoxA_t)myGetProcAddress(hUser32, messageboxa);
     pMessageBoxA(NULL, hello_msg, title_msg, MB_OK | MB_ICONINFORMATION);
 }
-
